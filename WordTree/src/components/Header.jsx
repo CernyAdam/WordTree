0b22a42e53af927e526d0d6500dd6ebbe3eab7b6
Adam Černý <adam1cerny@gmail.com>
2024-11-06 04:18:31 +0100

    Footer and Header

---
+++ WordTree/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>WordTree</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Sample</a></li>
+          <li><a href="#">Text</a></li>
+        </ul>
+      </nav>
+      <hr/>
+    </header>
+  );
+}
+
+export default Header;
