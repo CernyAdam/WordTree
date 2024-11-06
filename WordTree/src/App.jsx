89e1014b5daed390a19756124536b4372c0a467a
Adam Černý <adam1cerny@gmail.com>
2024-11-06 04:33:48 +0100

    Sample card

--- WordTree/src/App.jsx
+++ WordTree/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Card from './components/Card.jsx'
 
 function App() {
   return (
     <>
       <Header/>
+      <Card/>
       <Footer/>
     </>
   );
---
+++ WordTree/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img alt="Sample image" src="https://via.placeholder.com/200"></img>
+            <h2>Sample card</h2>
+            <p>Sample text</p>
+        </div>
+    );
+}
+
+export default Card;
